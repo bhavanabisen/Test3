0ff264248d1abeb62ff5b34d3d2ba9819d50a06e
lenovo <lenovo@LAPTOP-H679TKPN>
2022-10-04 19:59:36 +0530

    First commit

---
+++ src/Basic/Test1.java
@@ -0,0 +1,15 @@
+package Basic;
+
+public class Test1 
+{
+  public static void main(String[] args) 
+  {
+	System.out.println("Main method is running");
+  }
+  
+  public static void m1()
+  {
+	  System.out.println("Hii");
+  }
+  
+}
